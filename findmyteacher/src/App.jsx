fc51396cd99d6e15fd0934242796778b42fe635a
Mayank <mayankjainx404@gmail.com>
2024-03-04 16:57:26 +0530

    Added react router dom

---
+++ findmyteacher/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom'
+
+function App() {
+  return (
+    <Router>
+    </Router>
+  );
+}
+
+export default App;

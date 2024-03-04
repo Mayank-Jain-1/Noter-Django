05831f059350e4f8145561bd4ac5fe01ac9bc0ac
Mayank <mayankjainx404@gmail.com>
2024-03-04 17:37:24 +0530

    Added components

--- findmyteacher/src/App.jsx
+++ findmyteacher/src/App.jsx
@@ -1,11 +1,18 @@
-import './App.css';
-import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom'
+import "./App.css";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import Map from "./Components/Map";
 
 function App() {
-  return (
-    <Router>
-    </Router>
-  );
+	return (
+		<Router>
+			<Routes>
+				<Route path="/"></Route>
+				<Route path="/map" Component={Map}>
+
+        </Route>
+			</Routes>
+		</Router>
+	);
 }
 
 export default App;
